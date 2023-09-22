8aff3fb2e4e09eeecc8571d2c52d0bdbb466fa7f
bashir <bigbasho@gmail.com>
2023-09-22 07:09:53 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""Making change O(n)"""
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
+import sys
 
 
 def makeChange(coins, total):
-    """Clasic Bottom-Up dynamic programming"""
-    temp_value = 0
-   coins.sort(reverse=True)
-
-    if total < 0:
+    '''
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
+    if total <= 0:
         return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
-
-    return temp_value if total == 0 else -1
-
-
-if __name__ == '__main__':
-
-    print(makeChange([1, 2, 25], 37))
-
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
